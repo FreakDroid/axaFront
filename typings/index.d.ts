461bbe55883005341db70db7297485764c8882fb
Freakdroid <wilfredo1285@gmail.com>
2017-09-02 07:17:07 -0400

    Updated gitignore file

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/angular-ui-router/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/datatables.net/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
